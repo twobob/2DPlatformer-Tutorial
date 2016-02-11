3336d494b26b607e0f7f96226063e2c7b617c0de
Justin Garza <JGarza9788@gmail.com>
2016-02-10 18:43:59 -0800

    ButtonMapping, JumpMultiplier,SpeedMultiplier, WallJump struct
    ButtonMapping is now just a few strings, and key codes. Jump and Speed
    Multipliers can be used to easily create movement powers ups. WallJump
    variables can now be assigned with an Angle and a Force. …Also added a
    dash cool down time, so dashing cannot be done too quickly

--- 2D_Platerformer/Assets/Episode 11/Player.cs
+++ 2D_Platerformer/Assets/Episode 11/Player.cs
@@ -4,18 +4,24 @@ using System.Collections;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
-//    public ButtonNames buttonNames;
+    public ButtonNames buttonNames;
 
-    public float maxJumpHeight = 4;
-    public float minJumpHeight = 1;
+    public float maxJumpHeight = 4f;
+    public float minJumpHeight = 1f;
     public float timeToJumpApex = .4f;
+    public float jumpMuliplyer = 1f;
+
     float accelerationTimeAirborne = .2f;
     float accelerationTimeGrounded = .1f;
-    float moveSpeed = 6;
+    float moveSpeed = 6f;
+    float speedMultiplier = 1f;
 
-    public Vector2 wallJumpClimb;
-    public Vector2 wallJumpOff;
-    public Vector2 wallLeap;
+    public WallJump wallJumpClimb;
+    public WallJump wallJumpOff;
+    public WallJump wallLeap;
+    //public Vector2 wallJumpClimb;
+    //public Vector2 wallJumpOff;
+    //public Vector2 wallLeap;
 
     public float wallSlideSpeedMax = 3;
     public float wallStickTime = .25f;
@@ -24,22 +30,23 @@ public class Player : MonoBehaviour {
     float gravity;
     float maxJumpVelocity;
     float minJumpVelocity;
-    Vector3 velocity;
+    Vector2 velocity;
     float velocityXSmoothing;
 
     private float DownPressPrevTime = -1000f;
     private float DownPressFTP = 0.5f;
 
-    private Vector3 AdditionalVelocity;
-    public float AV_decelerationTimeGrounded;
-    public float AV_decelerationTimeAirborne;
+    private Vector2 additionalVelocity;
+    public float decelerationTimeGrounded_AV;
+    public float decelerationTimeAirborne_AV;
 
     public bool CanDash =  true;
-    public string DashButton = "Fire1";
     public float DashSpeed = 12;
-    private float DashPressPrevTime = -1000;
+    private float DashPressPrevTime = -1000f;
     private float DashPress = 0.5f;
     public float DashDuration = 0.25f;
+    public float DashCoolDown = 0.5f;
+    private float DashEndTime = -1000f;
     public bool IsDashing = false;
 
     public int AirBorneJumpCnt = 0;
@@ -47,17 +54,43 @@ public class Player : MonoBehaviour {
 
     Controller2D controller;
 
-//    [System.Serializable]
-//    public struct ButtonNames
-//    {
-//        public string Horizontal;// = "Horizontal";
-//        public string Vertical;// = "Vertical";
-//        public string Dash;// = "";
-//        public string Jump;// = "Jump";
-//    }
+    [System.Serializable]
+    public struct ButtonNames
+    {
+        public string Horizontal;
+        public string Vertical;
+        public KeyCode Right;
+        public KeyCode Left;
+        public KeyCode Down;
+        public KeyCode Up;
+        public string Dash;
+        public string Jump;
+    }
+
+    [System.Serializable]
+    public struct WallJump
+    {
+        public float Angle;
+        public float Force;
+        public Vector2 XY;
+    }
 
     void Start()
     {
+
+//        buttonNames.Horizontal = "Horizontal";
+//        buttonNames.Vertical = "Vertical";
+//        buttonNames.Right = KeyCode.D;
+//        buttonNames.Left = KeyCode.A;
+//        buttonNames.Down = KeyCode.S;
+//        buttonNames.Up = KeyCode.W;
+//        buttonNames.Dash = "Fire1";
+//        buttonNames.Jump = "Jump";
+
+        wallJumpClimb.XY = GetXYFromAngle(wallJumpClimb.Angle, wallJumpClimb.Force);
+        wallJumpOff.XY = GetXYFromAngle(wallJumpOff.Angle, wallJumpOff.Force);
+        wallLeap.XY = GetXYFromAngle(wallLeap.Angle, wallLeap.Force);
+
         controller = GetComponent<Controller2D> ();
 
         gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
@@ -68,15 +101,15 @@ public class Player : MonoBehaviour {
 
     void Update()
     {
-        Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+        Vector2 input = new Vector2(Input.GetAxisRaw(buttonNames.Horizontal), Input.GetAxisRaw(buttonNames.Vertical));
         int wallDirX = (controller.collisions.left) ? -1 : 1;
 
-        float Speed = (IsDashing) ? DashSpeed : moveSpeed;
+        float Speed = (IsDashing) ? (DashSpeed * speedMultiplier) : (moveSpeed * speedMultiplier);
         float targetVelocityX = input.x * Speed;
 
         if (IsDashing && Mathf.Abs(input.x) != 1)
         {
-            targetVelocityX = DashSpeed * controller.collisions.faceDir;
+            targetVelocityX = (DashSpeed * speedMultiplier) * controller.collisions.faceDir;
         }
 
         velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
@@ -115,45 +148,52 @@ public class Player : MonoBehaviour {
         if (CanDash)
         {
             if (
-                (Input.GetKeyDown(KeyCode.RightArrow) && controller.collisions.faceDir == 1)
-                || (Input.GetButtonDown(DashButton))
-                )
+                Time.time - DashEndTime >= DashCoolDown
+                && IsDashing == false
+            )
             {
-                if (Time.time - DashPressPrevTime <= DashPress)
+
+                if (
+                    (Input.GetKeyDown(buttonNames.Right) && controller.collisions.faceDir == 1)
+                    || (Input.GetButtonDown(buttonNames.Dash))
+                )
                 {
-                    IsDashing = true;
-                    Invoke("ResetIsDashing", DashDuration);
+                    if (Time.time - DashPressPrevTime <= DashPress)
+                    {
+                        IsDashing = true;
+                        Invoke("ResetIsDashing", DashDuration);
+                    }
+
+                    DashPressPrevTime = Time.time;
                 }
 
-                DashPressPrevTime = Time.time;
-            }
-
-            if (
-                (Input.GetKeyDown(KeyCode.LeftArrow) && controller.collisions.faceDir == -1)
-                || (Input.GetButtonDown(DashButton))
+                if (
+                    (Input.GetKeyDown(buttonNames.Left) && controller.collisions.faceDir == -1)
+                    || (Input.GetButtonDown(buttonNames.Dash))
                 )
-            {
-                if (Time.time - DashPressPrevTime <= DashPress)
                 {
-                    IsDashing = true;
-                    Invoke("ResetIsDashing", DashDuration);
-                }
+                    if (Time.time - DashPressPrevTime <= DashPress)
+                    {
+                        IsDashing = true;
+                        Invoke("ResetIsDashing", DashDuration);
+                    }
 
-                DashPressPrevTime = Time.time;
+                    DashPressPrevTime = Time.time;
+                }
             }
         }
-            
+
         bool fallingThroughPlatform = false;
 
         if (controller.collisions.below && controller.collisions.fallingThroughPlatformBelow)
         {
-            if (Input.GetKey(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKey(buttonNames.Down) && Input.GetButtonDown(buttonNames.Jump))
             {
                 controller.fallThroughPlatform();
                 fallingThroughPlatform = true;
             }
 
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetKeyDown(buttonNames.Down))
             {
                 if (Time.time - DownPressPrevTime <= DownPressFTP)
                 {
@@ -167,53 +207,53 @@ public class Player : MonoBehaviour {
         }
 
 
-        if (Input.GetButtonDown ("Jump"))
+        if (Input.GetButtonDown(buttonNames.Jump))
         {
             if (wallSliding)
             {
                 if (wallDirX == input.x)
                 {
-                    velocity.x = -wallDirX * wallJumpClimb.x;
-                    velocity.y = wallJumpClimb.y;
+                    velocity.x = -wallDirX * (wallJumpClimb.XY.x * jumpMuliplyer);
+                    velocity.y = (wallJumpClimb.XY.y * jumpMuliplyer);
                 }
                 else if (input.x == 0)
                 {
-                    velocity.x = -wallDirX * wallJumpOff.x;
-                    velocity.y = wallJumpOff.y;
+                    velocity.x = -wallDirX * (wallJumpClimb.XY.x * jumpMuliplyer);
+                    velocity.y = (wallJumpClimb.XY.y  * jumpMuliplyer);
                 }
                 else
                 {
-                    velocity.x = -wallDirX * wallLeap.x;
-                    velocity.y = wallLeap.y;
+                    velocity.x = -wallDirX * (wallJumpClimb.XY.x * jumpMuliplyer);
+                    velocity.y = (wallJumpClimb.XY.y * jumpMuliplyer);
                 }
             }
 
             if (controller.collisions.below && !fallingThroughPlatform )
             {
-                velocity.y = maxJumpVelocity;
+                velocity.y = (maxJumpVelocity * jumpMuliplyer);
             }
 
             if (!controller.collisions.below && !wallSliding)
             {
                 if (AirBorneJumpCnt < AirBorneMaxJumpCnt)
                 {
-                    velocity.y = maxJumpVelocity;
+                    velocity.y = (maxJumpVelocity * jumpMuliplyer);
                     AirBorneJumpCnt++;
                 }
             }
         }
 
-        if (Input.GetButtonUp ("Jump"))
+        if (Input.GetButtonUp(buttonNames.Jump))
         {
-            if (velocity.y > minJumpVelocity)
+            if (velocity.y > (minJumpVelocity * jumpMuliplyer))
             {
-                velocity.y = minJumpVelocity;
+                velocity.y = (minJumpVelocity * jumpMuliplyer);
             }
         }
 
         velocity.y += gravity * Time.deltaTime;
 
-        velocity += AdditionalVelocity;
+        velocity += additionalVelocity;
 
         controller.Move (velocity * Time.deltaTime, input);
 
@@ -227,30 +267,38 @@ public class Player : MonoBehaviour {
             AirBorneJumpCnt = 0;
         }
 
-        AdditionalVelocity.x = Mathf.Lerp(AdditionalVelocity.x, 0f, (controller.collisions.below) ? AV_decelerationTimeGrounded : AV_decelerationTimeAirborne);
-        AdditionalVelocity.y = Mathf.Lerp(AdditionalVelocity.y, 0f, (controller.collisions.below) ? AV_decelerationTimeGrounded : AV_decelerationTimeAirborne);
+        additionalVelocity.x = Mathf.Lerp(additionalVelocity.x, 0f, (controller.collisions.below) ? decelerationTimeGrounded_AV : decelerationTimeAirborne_AV);
+        additionalVelocity.y = Mathf.Lerp(additionalVelocity.y, 0f, (controller.collisions.below) ? decelerationTimeGrounded_AV : decelerationTimeAirborne_AV);
 
     }
 
     void AddForce(Vector3 Force)
     {
-        AdditionalVelocity += Force;
+        additionalVelocity += new Vector2(Force.x,Force.y);
     }
 
     void AddForce(Vector2 Force)
     {
-//        AdditionalVelocity += Force;
+        additionalVelocity += Force;
     }
 
-    void AddForce(float Degree, float Force)
+    void AddForce(float Angle, float Force)
     {
-        //new Vector2 F = GetVect....()
+        Vector2 F = GetXYFromAngle(Angle,Force);
+        additionalVelocity += F;
+    }
 
-        //AdditionalVelocity += F;
+    private Vector2 GetXYFromAngle(float Angle,float Radius)
+    {
+        float X = Radius * Mathf.Cos(Angle * 0.017453f);
+        float Y = Radius * Mathf.Sin(Angle * 0.017453f);
+
+        return new Vector2(X, Y);
     }
 
     void ResetIsDashing()
     {
         IsDashing = false;
+        DashEndTime = Time.time;
     }
 }
