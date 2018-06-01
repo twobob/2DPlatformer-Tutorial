d02f832f4b7645150b1f8f943f95ee5e6f7a67ad
Justin Garza <jgarza9788@gmail.com>
2018-05-31 18:42:53 -0700

    reorganizing files

---
+++ 2D_Platerformer/Assets/LowPolyNature/Scripts/ItemDragHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler ,IBeginDragHandler{
+
+    public InventoryItemBase Item { get; set; }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+       
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = Vector3.zero;
+    }
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+}
